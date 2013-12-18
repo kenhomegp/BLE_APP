87c74ffce501b7af2d57197f79fe743afa7fdeeb
kenhomegp <kenhomegp@yahoo.com.tw>
2013-12-18 11:02:51 +0800

    Initial Commit

---
+++ MerryBLEtool/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MerryBLEtool
+//
+//  Created by merry on 13-12-18.
+//  Copyright (c) 2013年 merry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MerryBLEtool/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MerryBLEtool
+//
+//  Created by merry on 13-12-18.
+//  Copyright (c) 2013年 merry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
